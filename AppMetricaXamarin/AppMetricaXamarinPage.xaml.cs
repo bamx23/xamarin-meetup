c11bcfc29ce707adbd8a24516071aa2dd5235900
Nikolay Volosatov <bamx23@yandex-team.ru>
2016-12-03 12:53:41 +0300

    Implement simple vote

--- AppMetricaXamarin/AppMetricaXamarinPage.xaml.cs
+++ AppMetricaXamarin/AppMetricaXamarinPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+using YandexMetricaPCL;
 
 namespace AppMetricaXamarin
 {
@@ -7,6 +9,37 @@ namespace AppMetricaXamarin
 		public AppMetricaXamarinPage()
 		{
 			InitializeComponent();
+
+			sendButton.IsEnabled = false;
+
+			voteView.IsVisible = true;
+			thanksLabel.IsVisible = false;
+		}
+
+		void PickerSelectedIndexChanged(object sender, System.EventArgs e)
+		{
+			sendButton.IsEnabled = true;
+		}
+
+		void SendButtonClicked(object sender, System.EventArgs e)
+		{
+			var results = new Dictionary<string, string>();
+			AddResult(results, "gender", genderPicker);
+			AddResult(results, "animal", animalPicker);
+			AddResult(results, "os", osPicker);
+
+			YandexMetrica.Implementation.ReportEvent("user-info", results);
+
+			voteView.IsVisible = false;
+			thanksLabel.IsVisible = true;
+		}
+
+		static void AddResult(Dictionary<string, string> results, string key, Picker picker)
+		{
+			if (picker.SelectedIndex < 0)
+				return;
+
+			results[key] = picker.Items[picker.SelectedIndex];
 		}
 	}
 }
