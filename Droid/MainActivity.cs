bf9a0ccaf36fa431ce3399cd6247bfa772832470
Nikolay Volosatov <bamx23@yandex-team.ru>
2016-12-05 16:00:35 +0300

    Lock portrait screen orientation

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 
 namespace AppMetricaXamarin.Droid
 {
-	[Activity(Label = "AppMetricaXamarin.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity(Label = "AppMetricaXamarin.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize, ScreenOrientation = ScreenOrientation.Portrait)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate(Bundle bundle)
